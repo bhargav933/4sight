b8e684d20716177763cebc0d6f2bbf4c55730dfb
vivektrivedi-123 <69842867+vivektrivedi-123@users.noreply.github.com>
2021-07-23 14:16:35 +0530

    Create git_pull.py

---
+++ git_pull.py
@@ -0,0 +1,28 @@
+import pexpect
+import os
+import sys
+import time
+
+user = 'root'
+password = sys.argv[1]
+cmd = "git pull origin dev"
+
+
+def shell_login():
+
+    child = pexpect.spawn(cmd, encoding='utf-8', echo=True)
+    child.expect("/id_rsa':")
+    child.sendline(password)
+    status = child.expect(pexpect.EOF, timeout=None)
+    print(status)
+
+
+time.sleep(5)
+os.chdir("/repo/openemr/")
+os.system("pwd")
+time.sleep(5)
+os.system("ls -l")
+time.sleep(5)
+shell_login()
+time.sleep(5)
+os.system("git log -1")
